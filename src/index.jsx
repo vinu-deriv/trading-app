5e200975f516fc2ad365ef00f807de10ad53ba4f
vinu-deriv <vinu@deriv.com>
2022-10-16 22:20:19 +0400

    feat: :art: PWA setup added

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,13 @@ import { render } from "solid-js/web";
 import "./index.scss";
 import App from "./App";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function () {
+    })
+    .catch(function (err) {
+    });
+}
+
 render(() => <App />, document.getElementById("root"));
