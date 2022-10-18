eaa2ff5d8498352bf30211f30124dfca9cdb4ab3
Likhith Kolayari <likhith@regentmarkets.com>
2022-10-18 15:47:25 +0400

    feat: :construction: incorporated PWA

--- src/index.jsx
+++ src/index.jsx
@@ -4,26 +4,26 @@ import { render } from "solid-js/web";
 import "./index.scss";
 import App from "./App";
 
-import logo48 from "./assets/icons/icon-48x48.png";
-import logo72 from "./assets/icons/icon-72x72.png";
-import logo96 from "./assets/icons/icon-96x96.png";
-import logo128 from "./assets/icons/icon-128x128.png";
-import logo144 from "./assets/icons/icon-144x144.png";
-import logo152 from "./assets/icons/icon-152x152.png";
-import logo192 from "./assets/icons/icon-192x192.png";
-import logo284 from "./assets/icons/icon-284x284.png";
-import logo512 from "./assets/icons/icon-512x512.png";
+// import logo48 from "./assets/icons/icon-48x48.png";
+// import logo72 from "./assets/icons/icon-72x72.png";
+// import logo96 from "./assets/icons/icon-96x96.png";
+// import logo128 from "./assets/icons/icon-128x128.png";
+// import logo144 from "./assets/icons/icon-144x144.png";
+// import logo152 from "./assets/icons/icon-152x152.png";
+// import logo192 from "./assets/icons/icon-192x192.png";
+// import logo284 from "./assets/icons/icon-284x284.png";
+// import logo512 from "./assets/icons/icon-512x512.png";
 
 render(() => <App />, document.getElementById("root"));
 
-export {
-  logo48,
-  logo72,
-  logo96,
-  logo128,
-  logo144,
-  logo152,
-  logo192,
-  logo284,
-  logo512,
-};
+// export {
+//   logo48,
+//   logo72,
+//   logo96,
+//   logo128,
+//   logo144,
+//   logo152,
+//   logo192,
+//   logo284,
+//   logo512,
+// };
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,86 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import { parse } from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Trading App",
+        short_name: "Trading App",
+        start_url: "./index.html",
+        display: "standalone",
+        background_color: "#000",
+        theme_color: "#000",
+        scope: ".",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "/src/assets/icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/src/assets/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/src/assets/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/src/assets/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/src/assets/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/src/assets/icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/src/assets/icons/icon-284x284.png",
+            sizes: "284x284",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
   build: {
     target: "esnext",
-    outDir: "./dist",
-    assetsDir: "./assets",
-    manifest: true,
-    rollupOptions: {
-      input: {
-        app: "/index.html",
-        sw: "/sw.js",
-      },
-      output: {
-        entryFileNames: (assetInfo) =>
-          assetInfo.name === "sw" ? "[name].js" : "assets/js/[name]-[hash].js",
-      },
-    },
   },
 });
