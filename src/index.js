41f8066771d821c8ac3195edd812f3e14fe55864
vinu-deriv <vinu@deriv.com>
2022-10-19 22:15:30 +0400

    fix: :wrench: fix the configuration for pwa, service worker and caching

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    // eslint-disable-next-line no-console
-    .then(() => console.log("Service worker registered"))
-    .catch(function (err) {});
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("/sw.js")
+//     // eslint-disable-next-line no-console
+//     .then(() => console.log("Service worker registered"))
+//     .catch(function (err) {});
+// }
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,122 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import solidPlugin from "vite-plugin-solid";
-import { parse } from "path";
+import { dependencies } from './package.json';
+
+function renderChunks(deps) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['@deriv/deriv-api', 'solid-js'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      base: "/",
+      // strategies: 'injectManifest',
+      // srcDir: 'src',
+      // filename: 'sw.js',
+      strategies: 'generateSW',
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /.*(js|css|ico|png|html|svg)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "project-files",
+              expiration: {
+                maxAgeSeconds: 60 * 60 * 24,
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: "Trading App",
+        short_name: "TradingApp",
+        description: "My Awesome App description",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icon-284x284.png",
+            sizes: "284x284",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+        /* other options */
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
   build: {
     target: "esnext",
-    outDir: "./dist",
-    assetsDir: "./assets",
-    manifest: true,
     rollupOptions: {
-      input: {
-        app: "/index.html",
-        sw: "/sw.js",
-      },
       output: {
-        entryFileNames: (assetInfo) =>
-          assetInfo.name === "sw" ? "[name].js" : "assets/js/[name]-[hash].js",
+        manualChunks: {
+          vendor: ['@deriv/deriv-api', 'solid-js'],
+          ...renderChunks(dependencies),
+        },
       },
     },
   },
+  
 });
