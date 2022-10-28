43851c28de18923cfda52be503b2823e75b7c186
Farrah Mae Ochoa <farrah@deriv.com>
2022-10-28 17:44:56 +0400

    navbar cleanup

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,36 +4,46 @@ import { loginUrl } from "Constants/deriv-urls";
 import { login_information, logout } from "Stores/base-store";
 
 const account_balance = 100000;
+
+const Sidebar = () => {
+  return (
+    <ul class={styles.menu}>
+      <li>Trade</li>
+      <li>Report</li>
+      {login_information.is_logged_in && <li onClick={logout}> Sign Out</li>}
+    </ul>
+  );
+};
+
+const NavBarHeader = () => {
+  return (
+    <>
+      {login_information.is_logged_in ? (
+        <button class={styles.account_info}>
+          <div class={styles.account_wrapper}>
+            {account_balance}
+            <span>USD</span>
+          </div>
+          <i class={styles.arrow_down} />
+        </button>
+      ) : (
+        <div
+          onClick={() => (window.location.href = loginUrl({ language: "en" }))}
+        >
+          Log In
+        </div>
+      )}
+    </>
+  );
+};
+
 const NavBar = () => {
   return (
     <>
       <div id={styles.desktop}>
         <section class={styles.topnav_desktop}>
-          <a href="#" class={styles.logo}>
-            <img src={Logo} class={styles.logo} />
-          </a>
-          <ul class={styles.menu}>
-            <li>Trade</li>
-            <li>Report</li>
-            {login_information.is_logged_in ? (
-              <li onClick={logout}> Sign Out</li>
-            ) : (
-              <li
-                onClick={() =>
-                  (window.location.href = loginUrl({ language: "en" }))
-                }
-              >
-                Log In
-              </li>
-            )}
-          </ul>
-          <button class={styles.account_info}>
-            <div class={styles.account_wrapper}>
-              {account_balance}
-              <span>USD</span>
-            </div>
-            <i class={styles.arrow_down} />
-          </button>
+          <Sidebar />
+          <NavBarHeader />
         </section>
       </div>
 
@@ -43,31 +53,8 @@ const NavBar = () => {
           <label class={styles.menu_button_container} for={styles.menu_toggle}>
             <div class={styles.menu_button} />
           </label>
-          <ul class={styles.menu}>
-            <li>Trade</li>
-            <li>Report</li>
-            {login_information.is_logged_in ? (
-              <li onClick={logout}> Sign Out</li>
-            ) : (
-              <li
-                onClick={() =>
-                  (window.location.href = loginUrl({ language: "en" }))
-                }
-              >
-                Log In
-              </li>
-            )}
-          </ul>
-          <a href="#" class={styles.logo}>
-            <img src={Logo} class={styles.logo} />
-          </a>
-          <button class={styles.account_info}>
-            <div class={styles.account_wrapper}>
-              {account_balance}
-              <span>USD</span>
-            </div>
-            <i class={styles.arrow_down} />
-          </button>
+          <Sidebar />
+          <NavBarHeader />
         </section>
       </div>
     </>
