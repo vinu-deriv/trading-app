a045c7e4830458a00c904fbccadc844ee28196f4
Likhith Kolayari <98398322+likhith-deriv@users.noreply.github.com>
2023-04-04 14:15:53 +0400

    likhith/feat: :sparkles: incorporated Route guard to handle routes (#97)
    * feat: :sparkles: incorporated Route guard to handle routes
    
    * feat: removed package-lock
    
    * feat: :bug: resolved import issue

--- src/routes/market-list/market-list.jsx
+++ src/routes/market-list/market-list.jsx
@@ -35,7 +35,7 @@ import { setSwipeDirection } from "Stores/ui-store";
 import shared from "Styles/shared.module.scss";
 import styles from "Styles/accordion.module.scss";
 import throttle from "lodash.throttle";
-import { useNavigate } from "solid-app-router";
+import { useNavigate } from "@solidjs/router";
 import { errorCatcher } from "Utils/error-handler";
 
 const MarketList = () => {
